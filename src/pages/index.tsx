5801e29f0fcf5fa43ce46fef4bd7e5fc10e06672
112441969+QM2000GG@users.noreply.github.com <112441969+QM2000GG@users.noreply.github.com>
2022-12-17 00:11:45 -0800

    fix final game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function HomePage() {
     console.log('flip!')
     //setCards to cards but the cards that was clicked should be flipped
 
-    const desiredResult: Array<any> = (cards.map((eachCard: any) => {
+    var desiredResult: Array<any> = (cards.map((eachCard: any) => {
 
       if (eachCard.id === urltoflip) {
         return {
@@ -66,6 +66,46 @@ export default function HomePage() {
       }
 
     }))
+
+    var numberOfCardsFlippedPerUrl: any = {}
+
+
+
+    var wonurl = ""
+
+    var wonurls = []
+
+
+    desiredResult.forEach((eachCard: any) => {
+      // add 1 to numberOfCardsFlippedPerUrl[eachCard.url] if it exists
+
+      if (eachCard.isflipped) {
+        if (numberOfCardsFlippedPerUrl[eachCard.url]) {
+          numberOfCardsFlippedPerUrl[eachCard.url] = numberOfCardsFlippedPerUrl[eachCard.url] + 1
+          wonurl = eachCard.url;
+          wonurls.push(eachCard.url)
+        }
+        else {
+          numberOfCardsFlippedPerUrl[eachCard.url] = 1
+        }
+      }
+
+    })
+
+    console.log('numberOfCardsFlippedPerUrl', numberOfCardsFlippedPerUrl)
+
+    desiredResult = desiredResult.map((eachCard: any) => {
+      if (eachCard.url == wonurl || wonurls.includes(eachCard.url)) {
+        return {
+          ...eachCard,
+          won: true
+        }
+      } else {
+        return eachCard
+      }
+    });
+
+    console.log('desired result', desiredResult)
 
     setCards(desiredResult)
 
@@ -80,20 +120,32 @@ export default function HomePage() {
 
       <main>
         { /*  create 10 cards on grid on screen using tailwind*/}
-
+        {cards.filter((eachCard: any) => eachCard.won === false).length === 0 && (
+          <>
+            <h1>You won!</h1><br></br></>
+        )}
         <div className="flex flex-row flex-wrap  gap-x-5 gap-y-4">
+
+
+
           {cards.map((eachCard: any, index: number) => {
             return (
-              <div className="bg-gray-300 w-40" key={index}
-
-
-
-
+              <div className="" key={index}
               >
-                <img src={eachCard.isflipped === true ? eachCard.url : backside} className=" w-40"
+                {
+                  eachCard.won ? (
+                    <div className="w-40 h-[280px] bg-green-200 border-green-500 border-2 w-40">
+                      <div className='my-auto text-2xl'>
+                        🎉
+                      </div>
+                    </div>
+                  ) : (
+                    <img src={eachCard.isflipped === true ? eachCard.url : backside} className=" w-40"
 
-                  onClick={(event: any) => flipCard(eachCard.id)}
-                />
+                      onClick={(event: any) => flipCard(eachCard.id)}
+                    />
+                  )
+                }
               </div>
             )
           })}
